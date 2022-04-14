c3ed34f0b549ebcf30da6c8ae0ead50ac5844483
PCristian00 <cristianp9300@gmail.com>
2022-04-14 18:01:55 +0200

    Completo

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 def sommatrice(lista):
-    ris=0
+    ris = 0
     for elem in lista:
-        ris+=elem
-    print("La somma di tutti gli elementi della lista è "+str(ris))
+        ris += elem
+    print("La somma di tutti gli elementi della lista è " + str(ris))
 
 
-num=int(input("Dimensione lista: "))
-arr=list()
+def moltiplicatrice(lista):
+    ris = 1
+    for elem in lista:
+        ris *= elem
+    print("Il prodotto di tutti gli elementi della lista è " + str(ris))
+
+
+num = int(input("Dimensione lista: "))
+arr = list()
 
 for i in range(num):
-    ele=int(input("Inserire elemento "+(str(i+1))+": "))
+    ele = int(input("Inserire elemento " + (str(i + 1)) + ": "))
     arr.append(ele)
 print(arr)
 sommatrice(arr)
+moltiplicatrice(arr)
